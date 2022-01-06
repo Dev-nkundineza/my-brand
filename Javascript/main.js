2f1d47429f0efd8648c7cdae9c0e9fc006257c9c
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-01-06 23:56:12 +0200

    the commit with complete pages and responsiveness

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,17 +1,31 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+// const mainMenu = document.querySelector('.navbar');
+// const closeMenu = document.querySelector('.closeMenu');
+// const openMenu = document.querySelector('.openMenu');
 
-openMenu.addEventListener("click ", show);
-closeMenu.addEventListener("click ", close);
+// openMenu.addEventListener("click ", show);
+// closeMenu.addEventListener("click ", close);
 
-function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+// function show() {
+//     mainMenu.style.display = 'flex';
+//     mainMenu.style.top = '0';
 
-}
+// }
 
-function close() {
+// function close() {
 
-    mainMenu.style.display = 'none ';
+//     mainMenu.style.left = '-100%';
+// }
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".nav");
+    nav.classList.toggle("open");
+    if (nav.classList.contains("open")) {
+        nav.style.maxHeight = nav.scrollHeight + "px";
+    } else {
+        nav.removeAttribute("style");
+    }
 }
